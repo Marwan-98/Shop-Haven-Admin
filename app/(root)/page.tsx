a04fb557f151ec39e46c0d88a29671fff302d91c
Marwan Ashraf <marwan.ashraf@scandiweb.com>
2023-12-25 10:39:20 +0200

    Clerk Auth & Admin Modal Component

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Modal } from "@/components/ui/modal";
+
+const SetupPage = () => {
+  return (
+    <div className="p-4">
+      <Modal isOpen onClose={() => {}} title="test" description="desc.">
+        Children
+      </Modal>
+    </div>
+  );
+};
+
+export default SetupPage;
