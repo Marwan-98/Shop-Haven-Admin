367ee199b3c1898bca070482cfb02e2e5e21a50f
Marwan Ashraf <marwan.ashraf@scandiweb.com>
2023-12-25 11:00:07 +0200

    Adding create store modal to root page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
+import React from 'react';
+
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import { Inter } from 'next/font/google';
+
+import ModalProvider from '@/providers/Modal-provider';
+
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,12 +18,15 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
