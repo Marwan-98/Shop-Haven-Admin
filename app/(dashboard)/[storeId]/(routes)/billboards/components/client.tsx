530169132b649dacd56da3c06fcd95b6aea5a6d1
Marwan Ashraf <marwan.ashraf@scandiweb.com>
2024-01-11 09:37:58 +0200

    Create Billboard form & routes

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -2,11 +2,15 @@
 
 import { Plus } from "lucide-react";
 import React from "react";
+import { useParams, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 
 const BillboardClient = () => {
+    const router = useRouter();
+    const params = useParams();
+
   return (
     <>
         <div className="flex items-center justify-between">
@@ -14,7 +18,7 @@ const BillboardClient = () => {
                 title="Billboard (0)"
                 description="Manage billboards for your store"
             />
-            <Button>
+            <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
                 <Plus className="mr-2 h-4 w-4"/>
                 Add New
             </Button>
